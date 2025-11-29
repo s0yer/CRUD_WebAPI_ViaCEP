596802e1f0141c1d61ae913d1a2c948ee038aeb2
jadsonmarliere <jds.mdo@gmail.com>
2025-11-29 16:31:43 -0300

    Create EnderecoAPI.cs

---
+++ Models/EnderecoAPI.cs
@@ -0,0 +1,19 @@
+﻿namespace CRUD_WebAPI_ViaCEP.Models
+{
+    public class EnderecoAPI
+    {
+        public int Id { get; set; }
+        public string Cep { get; set; } = string.Empty;
+        public string Logradouro { get; set; } = string.Empty;
+        public string Complemento { get; set; } = string.Empty;
+        public string Bairro { get; set; } = string.Empty;
+        public string Localidade { get; set; } = string.Empty; // cidade
+        public string Uf { get; set; } = string.Empty; // estado
+
+        // sobrescrita 
+        public override string ToString()
+        {
+            return $"{Logradouro}, {Bairro}, {Localidade}-{Uf} - CEP: {Cep}";
+        }
+    }
+}
