00625a9242db959d8be14f2faf213aada1513ed4
jadsonmarliere <jds.mdo@gmail.com>
2025-11-30 12:53:33 -0300

    create
    model fix, data dbcontext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CRUD_WebAPI_ViaCEP.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace CRUD_WebAPI_ViaCEP.Data
+{
+   
+
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            {
+            }
+
+            public DbSet<EnderecoAPI> Enderecos { get; set; }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                // Opcional: Configurações adicionais
+                base.OnModelCreating(modelBuilder);
+            }
+        }
+    
+}
--- Models/EnderecoAPI.cs
+++ Models/EnderecoAPI.cs
@@ -1,15 +1,34 @@
-﻿namespace CRUD_WebAPI_ViaCEP.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_WebAPI_ViaCEP.Models
 {
     public class EnderecoAPI
     {
+        [Key]
         public int Id { get; set; }
-        public string Cep { get; set; } = string.Empty;
-        public string Logradouro { get; set; } = string.Empty;
-        public string Complemento { get; set; } = string.Empty;
-        public string Bairro { get; set; } = string.Empty;
-        public string Localidade { get; set; } = string.Empty; // cidade
-        public string Uf { get; set; } = string.Empty; // estado
 
+        [Required]
+        [StringLength(8)]
+        public string Cep { get; set; } = string.Empty;
+
+        [Required]
+        public string Logradouro { get; set; } = string.Empty;
+
+        public string? Complemento { get; set; }
+
+        [Required]
+        public string Bairro { get; set; } = string.Empty;
+
+        [Required]
+        public string Localidade { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(2)]
+        public string Uf { get; set; } = string.Empty;
+
+        // Opcionais:
+        public string? Ibge { get; set; }
+        public string? Gia { get; set; }
         // sobrescrita 
         public override string ToString()
         {
